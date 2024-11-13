e97c01928ff9b8c37186f3b91da89df85061d125
shihpaggy <paggy.shih@gmail.com>
2024-11-12 23:32:34 -0300

    add prisma and database setup

---
+++ app/login/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+const LoginPage = () => {
+  return (
+    <div className="grid h-full grid-cols-2">
+      {/* ESQUERDA*/}
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem Vindo</h1>
+        <p className="text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <Button variant="outline">Fazer login ou criar conta</Button>
+      </div>
+      {/* DIREITA*/}
+      <div className="relative h-full w-full">
+        <Image
+          src="/login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,9 @@
+const TransactionsPage = () => {
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-500">transactions</h1>
+    </div>
+  );
+};
+
+export default TransactionsPage;
